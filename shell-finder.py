ca28722e9cdec94298546c263480cc56d57d485d
zeinlol <nickolaiborshchov@gmail.com>
2021-10-22 16:43:17 +0300

    refactor for python3

--- shell-finder.py
+++ shell-finder.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-# Author: @alvarodh5 | Blackdrake
+# Author: @alvarodh5 | Blackdrake | zeinlol
 
 #########################################################################################################
 #                                            Shell Finder Python                                        #
 #                                                                                                       #
 #                                        coded by Blackdrake for Underc0de                              #
+#                                               redone by zeinlol                                       #
 #                                                                                                       #
-#                                                05/06/2015                                             #
+#                                                    2021                                               #
 #                                                                                                       #
 #                                     Underc0de.org | twitter.com/alvarodh5                             #
+#                                     Zeinlol |  https://github.com/zeinlol                             #
 #                                                                                                       #
 #                                           Run this tool as root                                       #
 #########################################################################################################
@@ -17,7 +19,7 @@
 import os
 
 
-class color:
+class ColorCode:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
     DARKCYAN = '\033[36m'
@@ -30,98 +32,107 @@ class color:
     END = '\033[0m'
 
 
-print
-color.BLUE + " __ _          _ _     ___ _           _" + color.END
-print
-color.BLUE + "/ _\ |__   ___| | |   / __( )_ __   __| | ___ _ __ " + color.END
-print
-color.BLUE + "\ \| '_ \ / _ \ | |  / _\ | | '_ \ / _` |/ _ \ '__|" + color.END
-print
-color.BLUE + "_\ \ | | |  __/ | | / /   | | | | | (_| |  __/ | " + color.END
-print
-color.BLUE + "\__/_| |_|\___|_|_| \/    |_|_| |_|\__,_|\___|_| " + color.END
-print
-""
-print
-color.YELLOW + "By Blackdrake (@alvarodh5) para Underc0de" + color.END
-print
-""
-print
-color.GREEN + "Asegurese que los ficheros que detecta son ilegitimos antes de eliminarlos" + color.END
-print
-color.GREEN + "Tenga en cuenta, que dependiendo de la cantidad de ficheros que tenga en su servidor, el script tardara mas o menos" + color.END
-raw_input("[Pulse INTRO para continuar]")
-print
-color.GREEN + "Introduzca ruta del directorio donde aloja sus ficheros (Pulsa Intro para utilizar por defecto, /var/www/)" + color.END
-directorio = raw_input("")
-if (directorio == ""):
-    directorio = "/var/www/"
-else:
-    longitud = len(directorio) - 1
-    if not (directorio[longitud] == "/"):
-        directorio += "/"
+logo = f"{ColorCode.BLUE} __ _          _ _     ___ _           _{ColorCode.END}\n" \
+       f"{ColorCode.BLUE}/ _\ |__   ___| | |   / __( )_ __   __| | ___ _ __{ColorCode.END}\n" \
+       f"{ColorCode.BLUE}\\ \| '_ \ / _ \ | |  / _\ | | '_ \ / _` |/ _ \\ '__|'{ColorCode.END}\n" \
+       f"{ColorCode.BLUE}_\ \ | | |  __/ | | / /   | | | | | (_| |  __/ | {ColorCode.END}\n" \
+       f"{ColorCode.BLUE}\__/_| |_|\___|_|_| \/    |_|_| |_|\__,_|\___|_| {ColorCode.END}\n" \
+       f"{ColorCode.YELLOW}By Zeinlol forked from Blackdrake (@alvarodh5){ColorCode.END}"
 
-print
-color.BLUE + "Buscando Shells conocidas" + color.END + "" + color.RED
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep r57 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible R57 [PHP] --> rm -rf " $2}\'| uniq')
+warning = f"{ColorCode.GREEN}Make sure the files it detects are illegitimate before deleting them{ColorCode.END}" \
+          f"{ColorCode.GREEN}Keep in mind, that depending on the amount of files you have on your server, the " \
+          f"script will take more or less time{ColorCode.END}"
 
-os.system(
-    'find ' + directorio + ' -name "*".txt  -type f -print0  | xargs -0 grep r57 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible R57 [TXT] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep c99 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible C99 [PHP] --> rm -rf " $2}\' | uniq')
+def get_target() -> str:
+    target = input(f"{ColorCode.GREEN}Enter the path of the directory where you host your files "
+                   f"(Press Enter to use /var/www/ by default):{ColorCode.END}")
+    if target == "":
+        target = "/var/www/"
+    elif target[-1:] != "/":
+        target += "/"
+    print(f'{target=}')
+    return target
 
-os.system(
-    'find ' + directorio + ' -name "*".txt  -type f -print0  | xargs -0 grep c99 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible C99 [TXT] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep c100 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible C100 [PHP] --> rm -rf " $2}\' | uniq')
+def look_for_shells(target_folder: str):
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep r57 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible R57 [PHP] --> rm -rf " $2}\'| uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".txt  -type f -print0  | xargs -0 grep c100 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible C100 [TXT] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".txt  -type f -print0  | xargs -0 grep r57 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible R57 [TXT] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep c98 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible C98 [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep c99 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible C99 [PHP] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".txt  -type f -print0  | xargs -0 grep c98 | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible C98 [TXT] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".txt  -type f -print0  | xargs -0 grep c99 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible C99 [TXT] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep SE3lRVER | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible WEEVELY SHELL [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep c100 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible C100 [PHP] --> rm -rf " $2}\' | uniq')
 
-print
-color.END + color.BLUE + "Otros posibles casos de Shell" + color.END + "" + color.YELLOW
+    os.system('find ' + target_folder + ' -name "*".txt  -type f -print0  | xargs -0 grep c100 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible C100 [TXT] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep shell | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep c98 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible C98 [PHP] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep backdoor | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".txt  -type f -print0  | xargs -0 grep c98 | uniq -c  | sort -u  |'
+                                        ' cut -d":" -f1  | awk \'{print "Possible C98 [TXT] --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep stealth | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep SE3lRVER | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible WEEVELY SHELL [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
+    print(f"{ColorCode.END}{ColorCode.BLUE}Other possible Shell cases:{ColorCode.END}{ColorCode.YELLOW}")
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep shell | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep exec | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep backdoor | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep popen | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep stealth | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep proc_open | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep exec | uniq -c  | '
+                                        'sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP] --> '
+                                        'rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep symlink | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep popen | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep passthru | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep proc_open | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP] --> rm -rf'
+                                        ' " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep system | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep symlink | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
 
-os.system(
-    'find ' + directorio + ' -name "*".php  -type f -print0  | xargs -0 grep milw0rm | uniq -c  | sort -u  | cut -d":" -f1  | awk \'{print "Posible Shell [PHP] --> rm -rf " $2}\' | uniq')
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep passthru | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
 
-print
-color.END + color.GREEN + "Escaner finalizado, siguenos! @underc0de @alvarodh5 " + color.END
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep system | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
+
+    os.system('find ' + target_folder + ' -name "*".php  -type f -print0  | xargs -0 grep milw0rm | uniq -c  |'
+                                        ' sort -u  | cut -d":" -f1  | awk \'{print "Possible Shell [PHP]'
+                                        ' --> rm -rf " $2}\' | uniq')
+
+
+def main():
+    print(logo)
+    print(warning)
+    target = get_target()
+    print(f'{ColorCode.BLUE}Start looking for known Shells...{ColorCode.END}{ColorCode.RED}')
+    look_for_shells(target)
+    print(f'{ColorCode.END}{ColorCode.GREEN}Scanner finished, follow us! @underc0de @alvarodh5 @zeinlol{ColorCode.END}')
+
+
+if __name__ == "__main__":
+    main()
