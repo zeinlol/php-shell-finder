860e49e7a4274fc841e2a2a8859a487ba8f5b8fd
zeinlol <nickolaiborshchov@gmail.com>
2021-10-25 19:07:38 +0300

    update arguments.py

--- arguments.py
+++ arguments.py
@@ -3,12 +3,14 @@ import argparse
 parser = argparse.ArgumentParser(description='Tool for finding possible shells.')
 
 parser.add_argument('--no-color', default=False, action='store_const', const=True,
-                    help='Print text without colors. Default is False.')
+                    help='Print text without colors')
 parser.add_argument('--clean-output', default=True, action='store_const', const=False,
-                    help="Don't print logo and other text. Just found data. Default is False.")
+                    help="Don't print logo and other text. Just found data")
 parser.add_argument('-t', '--target', default='',
-                    help="Target folder with files for scan.")
+                    help="Target folder with files for scan. If not indicated tool will ask to specify folder")
 parser.add_argument('--json', default=False, action='store_const', const=True,
-                    help='Print text without colors. Default is False.')
+                    help='Print output as json')
+parser.add_argument('-o', '--output', default='',
+                    help='Print output to OUTPUT file')
 
 args = parser.parse_args()
