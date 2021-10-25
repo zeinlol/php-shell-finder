d85447d364f8fb0e4a5858802cd3163c085fd3e0
zeinlol <nickolaiborshchov@gmail.com>
2021-10-25 19:33:39 +0300

    add json support

--- sources/middleware.py
+++ sources/middleware.py
@@ -1,3 +1,5 @@
+import json
+
 from .text_variables import get_target_text, color
 
 
@@ -25,13 +27,26 @@ def print_found_data(shells: list[str]) -> None:
         print(shell)
 
 
-def convert_data_to_json(shells: list[str]) -> dict:
+def convert_data_to_dict(shells: list[str]) -> list[dict]:
+    formatted_shells = []
     for shell in shells:
-        print(shell)
+        shell = shell.split(':')
 
-    return {}
+        if len(shell) > 4:  # if ':' in code script will cat part of code, this IF fix this issue
+            shell[3] = ''.join(shell[3:])
+
+        dict_shell = {
+            'title': shell[0],
+            'file': shell[1][1:],
+            'code_line': shell[2],
+            'code': shell[3],
+        }
+        formatted_shells.append(dict_shell)
+    # print(formatted_shells)
+
+    return formatted_shells
 
 
 def print_data_as_json(shells: list[str]) -> None:
-    shells = convert_data_to_json(shells)
-    print(shells)
+    shells = convert_data_to_dict(shells)
+    print(json.dumps(shells))
